a45fff0372dba6bec36b0f895ff3ac56e8439785
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-15 11:25:44 -0500

    commit tree 27f043654c2fbf3a403c67acb3f339e7fbdbe580

--- src/App.js
+++ src/App.js
@@ -1,1 +1,33 @@
-// create your App component here
+import React from 'react'
+
+export default class App extends React.Component {
+
+  state = {
+    peopleInSpace: []
+  }
+
+  displayPeople() {
+    return this.state.peopleInSpace.map(p => <p>{p.name}</p>)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>People in Space</h1>
+        <ul>
+          {this.displayPeople()}
+        </ul>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        peopleInSpace: data.people
+      })
+    })
+  }
+}
